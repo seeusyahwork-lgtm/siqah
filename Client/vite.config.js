620d7a605fd5e9793e21c2076b46aaf1dc7f2ad1
seeusyahwork-lgtm <seeusyahwork@gmail.com>
2025-09-06 16:15:38 +0700

    Update vite.config.js
    coba lagi ke 5

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/siqah/', // bukan /Client/, cukup /siqah/
+  base: '/siqah/Client', // bukan /Client/, cukup /siqah/
   plugins: [react(), tailwindcss()],
 })
