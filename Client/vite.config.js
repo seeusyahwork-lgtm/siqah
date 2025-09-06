3692cc40b600ad81bf0caf964f8ca7f865ce1ee0
seeusyahwork-lgtm <seeusyahwork@gmail.com>
2025-09-06 15:55:32 +0700

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/siqah/Client/', // 👈 penting untuk GitHub Pages
+  base: '/siqah/Client/',
+
 })
